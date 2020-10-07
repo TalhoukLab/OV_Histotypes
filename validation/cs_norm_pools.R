92539d7bccb05fe7a948ef2315d6c218939b9016
Derek <dchiu@bccrc.ca>
2020-10-07 16:03:50 -0700

    combine pools normalization for CS2 and CS3

--- validation/cs_norm_pools.R
+++ validation/cs_norm_pools.R
@@ -1,4 +1,8 @@
-# Pool Method -------------------------------------------------------------
+# Setup -------------------------------------------------------------------
+
+source(here::here("validation/cs_process_cohorts.R"))
+
+# Pools Method ------------------------------------------------------------
 
 # Pool set: pool samples from CS2
 pool_samples <-
@@ -19,6 +23,18 @@ cs2_pools_mgx <- weights %>%
 cs3_pools_mgx <-
   tibble::enframe(rowMeans(ref_pools), name = "Name", value = "cs3_exp")
 
+# Extract cs2 norm counts (not pools)
+cs2_norm_counts <- cs2_norm %>% dplyr::select(-dplyr::one_of(names(cs2_pools)[-1]))
+
+# Normalize each gene by adding batch effect (diff in mean gx)
+cs2_normalized_data_pools <-
+  dplyr::inner_join(cs3_pools_mgx, cs2_pools_mgx, by = "Name") %>%
+  dplyr::transmute(Name, be = cs3_exp - cs2_exp) %>%
+  dplyr::inner_join(cs2_norm_counts, by = "Name") %>%
+  tidyr::gather(FileName, exp, -1:-4) %>%
+  dplyr::transmute(Name = forcats::fct_inorder(Name), FileName, exp = be + exp) %>%
+  tidyr::spread(Name, exp)
+
 # Extract cs3 norm counts (not pools)
 cs3_norm_counts <- cs3_norm %>% dplyr::select(-dplyr::one_of(names(ref_pools)))
 
@@ -34,4 +50,5 @@ cs3_normalized_data_pools <-
 ## Summary
 # CS2 pools: 9 samples, 365 genes # dim(t(cs2_pools))
 # CS3 pools: 22 samples, 513 genes # dim(t(ref_pools))
+# CS2 normalized: 1214 samples (1223 original - 9 from pools), 136 common genes # dim(cs2_normalized_data_pools)
 # CS3 normalized: 1295 samples (1317 original - 22 from pools), 136 common genes # dim(cs2_normalized_data_pools)
