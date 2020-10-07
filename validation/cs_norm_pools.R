f14063514bde7c4493b3a2e355c977ee4ecf0a45
Derek <dchiu@bccrc.ca>
2020-10-07 16:05:25 -0700

    rm pkg namespace qualifiers

--- validation/cs_norm_pools.R
+++ validation/cs_norm_pools.R
@@ -7,45 +7,45 @@ source(here::here("validation/cs_process_cohorts.R"))
 # Pool set: pool samples from CS2
 pool_samples <-
   gsub("-|\\.RCC", "", grep("POOL", pools[["CS2-FileName"]], value = TRUE))
-cs2_pools <- dplyr::select(cs2_norm, Name, paste0("X", pool_samples))
+cs2_pools <- select(cs2_norm, Name, paste0("X", pool_samples))
 
 # Locked down reference pools weights
 weights <- c("Pool1", "Pool2", "Pool3") %>%
-  purrr::set_names() %>%
-  purrr::map_dbl(~ mean(grepl(., names(ref_pools), ignore.case = TRUE)))
+  set_names() %>%
+  map_dbl(~ mean(grepl(., names(ref_pools), ignore.case = TRUE)))
 
 # Weighted mean gene expression for CS2 pools (using reference pools weights)
 cs2_pools_mgx <- weights %>%
-  purrr::imap_dfc(~ .x * rowMeans(dplyr::select(cs2_pools, dplyr::matches(.y)))) %>%
-  dplyr::transmute(Name = cs2_pools[["Name"]], cs2_exp = rowSums(.))
+  imap_dfc(~ .x * rowMeans(select(cs2_pools, matches(.y)))) %>%
+  transmute(Name = cs2_pools[["Name"]], cs2_exp = rowSums(.))
 
 # Mean gene expression for CS3 reference pools
 cs3_pools_mgx <-
-  tibble::enframe(rowMeans(ref_pools), name = "Name", value = "cs3_exp")
+  enframe(rowMeans(ref_pools), name = "Name", value = "cs3_exp")
 
 # Extract cs2 norm counts (not pools)
-cs2_norm_counts <- cs2_norm %>% dplyr::select(-dplyr::one_of(names(cs2_pools)[-1]))
+cs2_norm_counts <- cs2_norm %>% select(-one_of(names(cs2_pools)[-1]))
 
 # Normalize each gene by adding batch effect (diff in mean gx)
 cs2_normalized_data_pools <-
-  dplyr::inner_join(cs3_pools_mgx, cs2_pools_mgx, by = "Name") %>%
-  dplyr::transmute(Name, be = cs3_exp - cs2_exp) %>%
-  dplyr::inner_join(cs2_norm_counts, by = "Name") %>%
-  tidyr::gather(FileName, exp, -1:-4) %>%
-  dplyr::transmute(Name = forcats::fct_inorder(Name), FileName, exp = be + exp) %>%
-  tidyr::spread(Name, exp)
+  inner_join(cs3_pools_mgx, cs2_pools_mgx, by = "Name") %>%
+  transmute(Name, be = cs3_exp - cs2_exp) %>%
+  inner_join(cs2_norm_counts, by = "Name") %>%
+  gather(FileName, exp, -1:-4) %>%
+  transmute(Name = fct_inorder(Name), FileName, exp = be + exp) %>%
+  spread(Name, exp)
 
 # Extract cs3 norm counts (not pools)
-cs3_norm_counts <- cs3_norm %>% dplyr::select(-dplyr::one_of(names(ref_pools)))
+cs3_norm_counts <- cs3_norm %>% select(-one_of(names(ref_pools)))
 
 # Normalize each gene by adding batch effect (diff in mean gx)
 cs3_normalized_data_pools <-
-  dplyr::inner_join(cs2_pools_mgx, cs3_pools_mgx, by = "Name") %>%
-  dplyr::transmute(Name, be = cs2_exp - cs3_exp) %>%
-  dplyr::inner_join(cs3_norm_counts, by = "Name") %>%
-  tidyr::gather(FileName, exp, -1:-4) %>%
-  dplyr::transmute(Name = forcats::fct_inorder(Name), FileName, exp = be + exp) %>%
-  tidyr::spread(Name, exp)
+  inner_join(cs2_pools_mgx, cs3_pools_mgx, by = "Name") %>%
+  transmute(Name, be = cs2_exp - cs3_exp) %>%
+  inner_join(cs3_norm_counts, by = "Name") %>%
+  gather(FileName, exp, -1:-4) %>%
+  transmute(Name = fct_inorder(Name), FileName, exp = be + exp) %>%
+  spread(Name, exp)
 
 ## Summary
 # CS2 pools: 9 samples, 365 genes # dim(t(cs2_pools))
