9794863825d47a1a9a44427206b6966164c4b5d6
Derek <dchiu@bccrc.ca>
2021-02-16 11:29:22 -0800

    increase cpus per task and mem per cpu to try and minimize R_TempDir memory errors

--- pipeline/assets/sbatch_params.sh
+++ pipeline/assets/sbatch_params.sh
@@ -4,7 +4,7 @@
 #SBATCH --partition=upgrade       # Partition requested (queue)
 #SBATCH --nodes=1                 # Run all processes on a single node
 #SBATCH --ntasks=1                # Run a single task
-#SBATCH --cpus-per-task=4         # Number of CPU cores per task
-#SBATCH --mem=4gb                 # Job memory request
+#SBATCH --cpus-per-task=8         # Number of CPU cores per task
+#SBATCH --mem_per_cpu=4GB         # Memory per core
 #SBATCH --output=o_%j.out         # Standard output
 #SBATCH --error=e_%j.out          # Standard error
