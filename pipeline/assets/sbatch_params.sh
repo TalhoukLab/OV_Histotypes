daca7d2468d0d8a3407e7ae86220b7b9d160045c
Derek <dchiu@bccrc.ca>
2021-02-16 11:42:39 -0800

    should be dashes in option name

--- pipeline/assets/sbatch_params.sh
+++ pipeline/assets/sbatch_params.sh
@@ -5,6 +5,6 @@
 #SBATCH --nodes=1                 # Run all processes on a single node
 #SBATCH --ntasks=1                # Run a single task
 #SBATCH --cpus-per-task=8         # Number of CPU cores per task
-#SBATCH --mem_per_cpu=4GB         # Memory per core
+#SBATCH --mem-per-cpu=4GB         # Memory per core
 #SBATCH --output=o_%j.out         # Standard output
 #SBATCH --error=e_%j.out          # Standard error
