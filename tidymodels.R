a4528b10aaceac7b5a2d7c8cfb0a3ac5530052e4
Derek Chiu <dchiu@bccrc.ca>
2024-03-05 12:00:22 -0800

    split test set results and vi into separate merge script, specify output file name

--- tidymodels.R
+++ tidymodels.R
@@ -1,10 +1,11 @@
 # Server constants
+outputDir <- "~/results/OV_Histotypes/outputs"
 inputDir <- "data"
 dataset <- "train"
-fold_id <- 1
 rank_metric <- "accuracy"
 alg <- "rf"
 samp <- "hybrid"
+fold_id <- "01"
 
 # Load packages and data
 suppressPackageStartupMessages({
@@ -169,106 +170,11 @@ unregister_dopar <- function() {
 }
 unregister_dopar()
 
-# Ranking workflows for selected metric
-ranking <- tuned_set %>%
-  rank_results(rank_metric = rank_metric, select_best = TRUE) %>%
-  filter(.metric == rank_metric)
-
-# Best workflow
-best_wflow <- ranking[["wflow_id"]][1]
-
-# Best tuning parameters of workflow with highest metric
-best_params <- tuned_set %>%
-  extract_workflow_set_result(best_wflow) %>%
-  select_best(metric = rank_metric)
-
-# Finalize model with best set of tuning parameters
-best_model <- tuned_set %>%
-  extract_workflow(best_wflow) %>%
-  finalize_workflow(best_params)
-
-# Train best model on training set, evaluate on test set
-test_results <- best_model %>%
-  last_fit(split = outer_fold, metrics = mset)
-
-# Test set predictions
-test_preds <- test_results %>%
-  collect_predictions()
-
-# Calculate overall metrics
-overall_metrics <- test_results %>%
-  collect_metrics() %>%
-  select(-.config) %>%
-  add_column(class_group = "Overall")
-
-# Calculate per-class metrics using one-vs-all predictions
-per_class_mset <- metric_set(accuracy, f_meas, kap, gmean)
-per_class_metrics <- test_results %>%
-  collect_predictions() %>%
-  mutate(
-    pred_max = pmap_dbl(select(., matches(".pred") & where(is.double)),
-                        ~ pmap_dbl(list(...), max)),
-    across(matches(".pred") & where(is.double),
-           ~ factor(
-             ifelse(.x == pred_max, as.character(.pred_class), "class_0")
-           ),
-           .names = "{gsub('.pred_', '.pred_class_', {col}, fixed = TRUE)}"),
-    across(matches(".pred_class_.*"),
-           ~ factor(
-             ifelse(class %in% levels(.x), as.character(class), "class_0")
-           ),
-           .names = "{gsub('.pred_', '', {col}, fixed = TRUE)}")
-  ) %>%
-  pivot_longer(
-    matches("^.pred_class_.*"),
-    names_to = ".pred_class_group",
-    names_prefix = ".pred_class_",
-    values_to = ".pred_class_value"
-  ) %>%
-  pivot_longer(
-    matches("^class_.*"),
-    names_to = "class_group",
-    names_prefix = "class_",
-    values_to = "class_value"
-  ) %>%
-  filter(class_group == .pred_class_group) %>%
-  nest(.by = class_group) %>%
-  mutate(
-    data = data %>%
-      map(droplevels) %>%
-      map(per_class_mset, truth = class_value, estimate = .pred_class_value)
-  ) %>%
-  unnest(cols = data)
-
-# Combine all metrics
-all_metrics <- bind_rows(overall_metrics, per_class_metrics) %>%
-  arrange(.metric)
-
-# Top class by selected metric
-top_class <- all_metrics %>%
-  filter(.metric == rank_metric, class_group != "Overall") %>%
-  slice_max(order_by = .estimate)
-
-# Variable importance metrics
-## Use model-specific metrics if available, otherwise calculate
-## permutation-based variable importance
-if (alg %in% c("rf", "xgb", "mr")) {
-  vi_df <- test_results %>%
-    extract_fit_parsnip() %>%
-    vi()
-} else if (alg %in% "svm") {
-  svm_pfun <- function(object, newdata) {
-    kernlab::predict(object, new_data = newdata)[[".pred_class"]]
-  }
-  set.seed(2024)
-  vi_df <- test_results %>%
-    extract_fit_parsnip() %>%
-    vi(
-      train = analysis(outer_fold),
-      method = "permute",
-      target = "class",
-      metric = "accuracy",
-      nsim = 5,
-      pred_wrapper = svm_pfun
-    )
-}
+# Write tuned workflow to file
+results_file <- file.path(
+  outputDir,
+  "tune_wflows",
+  dataset,
+  paste0(samp, "_", alg, "_", fold_id, "_", dataset, ".rds")
+)
+saveRDS(tuned_set, results_file)
