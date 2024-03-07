ec5c0112797c6125178951a5757f8fa6887e0313
Derek Chiu <dchiu@bccrc.ca>
2024-03-06 18:08:01 -0800

    aggregate results by wflow not by fold

--- pipeline/2-merge_results.sh
+++ pipeline/2-merge_results.sh
@@ -15,29 +15,32 @@ for dataset in "${dataSets[@]}"; do
     mkdir -p $shSubDir/$dataset
     mkdir -p $outputDir/$subDir/$dataset
 
-    for v in $(seq -f "%0${#n_folds}g" 1 $n_folds); do
-        # Content of R file
-        R_file=$RSubDir/$dataset/"merge_results_"$v.R
-        echo 'dataset <- "'$dataset'"' > $R_file
-        echo "n_folds <- $n_folds" >> $R_file
-        echo 'fold_id <- "'$v'"' >> $R_file
-        echo 'rank_metric <- "'$rank_metric'"' >> $R_file
-        echo 'inputDir <- "'$inputDir'"' >> $R_file
-        echo 'outputDir <- "'$outputDir'"' >> $R_file
-        echo 'source("pipeline/2-merge_results.R")' >> $R_file
+    for alg in "${algs[@]}"; do
+        for samp in "${samps[@]}"; do
+            # Content of R file
+            R_file=$RSubDir/$dataset/$samp"_"$alg.R
+            echo 'dataset <- "'$dataset'"' > $R_file
+            echo "n_folds <- $n_folds" >> $R_file
+            echo 'alg <- "'$alg'"' >> $R_file
+            echo 'samp <- "'$samp'"' >> $R_file
+            echo 'rank_metric <- "'$rank_metric'"' >> $R_file
+            echo 'inputDir <- "'$inputDir'"' >> $R_file
+            echo 'outputDir <- "'$outputDir'"' >> $R_file
+            echo 'source("pipeline/2-merge_results.R")' >> $R_file
 
-        # Content of sh file
-        job_file=$shSubDir/$dataset/"merge_results_"$v.sh
-        cat ./assets/sbatch_params.sh > $job_file
-        echo "cd $projDir" >> $job_file
-        echo "Rscript $R_file" >> $job_file
-        chmod +x $job_file
+            # Content of sh file
+            job_file=$shSubDir/$dataset/$samp"_"$alg.sh
+            cat ./assets/sbatch_params.sh > $job_file
+            echo "cd $projDir" >> $job_file
+            echo "Rscript $R_file" >> $job_file
+            chmod +x $job_file
 
-        # Add to queue if sbatch exists
-        if command -v sbatch &>/dev/null; then
-            file_to_submit+=($job_file)
-            echo -e "$GREEN_TICK Added to queue: $job_file"
-        fi
+            # Add to queue if sbatch exists
+            if command -v sbatch &>/dev/null; then
+                file_to_submit+=($job_file)
+                echo -e "$GREEN_TICK Added to queue: $job_file"
+            fi
+        done
     done
 done
 
