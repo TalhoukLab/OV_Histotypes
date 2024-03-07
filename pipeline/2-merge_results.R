735da172dfc0a3e9efc0ce04c5243ebd64b1763d
Derek Chiu <dchiu@bccrc.ca>
2024-03-06 18:09:19 -0800

    aggregate metrics and var imp by wflow

--- pipeline/2-merge_results.R
+++ pipeline/2-merge_results.R
@@ -1,28 +1,37 @@
 # Load packages and data
 suppressPackageStartupMessages({
   library(dplyr)
+  library(purrr)
+  library(rlang)
   library(tidymodels)
   library(here)
 })
 source(here("src/funs.R"))
 source(here("pipeline/0-setup_data.R"))
 
-# Outer fold used for assessment
-id <- as.numeric(fold_id)
-outer_fold <- folds$splits[[id]]
-
-# Combine tuned workflows for each outer fold
-tuned_set <- list.files(
-  path = file.path(outputDir,
-                   "tune_wflows",
-                   dataset),
-  pattern = paste0("_", fold_id, "_", dataset),
+# Combined tuned workflow across folds
+wflow <- paste(samp, alg, sep = "_")
+tune_wflows_files <- list.files(
+  path = file.path(outputDir, "tune_wflows", dataset),
+  pattern = wflow,
   full.names = TRUE
-) %>%
-  map(readRDS) %>%
-  list_rbind()
+)
 
-# Ranking workflows for selected metric
+tuned_set <- tune_wflows_files %>%
+  map(readRDS) %>%
+  do.call(rbind, .)
+tuned_set$wflow_id <- paste0(tuned_set$wflow_id, seq_len(n_folds))
+
+# training_metrics <- tuned_set %>%
+#   collect_metrics() %>%
+#   summarize(
+#     mean_estimate = mean(mean),
+#     sd = sd(mean),
+#     lower = mean_estimate - qnorm(0.975) * sd,
+#     upper = mean_estimate + qnorm(0.975) * sd,
+#     .by = .metric
+#   )
+
 ranking <- tuned_set %>%
   rank_results(rank_metric = rank_metric, select_best = TRUE) %>%
   filter(.metric == rank_metric)
@@ -40,38 +49,36 @@ best_model <- tuned_set %>%
   extract_workflow(best_wflow) %>%
   finalize_workflow(best_params)
 
-# Train best model on training set, evaluate on test set
-test_results <- best_model %>%
-  last_fit(split = outer_fold, metrics = mset)
+# Train best model on outer training sets, evaluate on outer test sets
+outer_folds <- folds$splits
+test_results <- map(outer_folds, ~ {
+  last_fit(best_model, split = .x, metrics = mset)
+})
 
 # Test set predictions
 test_preds <- test_results %>%
-  collect_predictions()
+  map(collect_predictions) %>%
+  list_rbind(names_to = "fold_id")
 
 # Calculate overall metrics
-overall_metrics <- test_results %>%
-  collect_metrics() %>%
-  select(-.config) %>%
+overall_metrics <- test_preds %>%
+  mset(truth = class, .pred_CCOC:.pred_MUC, estimate = .pred_class) %>%
   add_column(class_group = "Overall")
 
 # Calculate per-class metrics using one-vs-all predictions
 per_class_mset <- metric_set(accuracy, f_meas, kap, gmean)
-per_class_metrics <- test_results %>%
-  collect_predictions() %>%
+per_class_metrics <- test_preds %>%
   mutate(
-    pred_max = pmap_dbl(select(., matches(".pred") & where(is.double)),
-                        ~ pmap_dbl(list(...), max)),
-    across(matches(".pred") & where(is.double),
-           ~ factor(
-             ifelse(.x == pred_max, as.character(.pred_class), "class_0")
-           ),
-           .names = "{gsub('.pred_', '.pred_class_', {col}, fixed = TRUE)}"),
-    across(matches(".pred_class_.*"),
-           ~ factor(
-             ifelse(class %in% levels(.x), as.character(class), "class_0")
-           ),
-           .names = "{gsub('.pred_', '', {col}, fixed = TRUE)}")
+    pred_class_ova = map(.pred_class, ~ {
+      ifelse(levels(.pred_class) %in% .x, as.character(.x), "class_0") %>%
+        set_names(paste0(".pred_class_", levels(.pred_class)))
+    }),
+    class_ova = map(class, ~ {
+      ifelse(levels(class) %in% .x, as.character(.x), "class_0") %>%
+        set_names(paste0("class_", levels(class)))
+    })
   ) %>%
+  unnest_wider(col = c(pred_class_ova, class_ova)) %>%
   pivot_longer(
     matches("^.pred_class_.*"),
     names_to = ".pred_class_group",
@@ -88,7 +95,7 @@ per_class_metrics <- test_results %>%
   nest(.by = class_group) %>%
   mutate(
     data = data %>%
-      map(droplevels) %>%
+      map(~ mutate(.x, across(matches("class_value"), factor))) %>%
       map(per_class_mset, truth = class_value, estimate = .pred_class_value)
   ) %>%
   unnest(cols = data)
@@ -106,24 +113,39 @@ top_class <- all_metrics %>%
 ## Use model-specific metrics if available, otherwise calculate
 ## permutation-based variable importance
 if (grepl("_(rf|xgb|mr)", best_wflow)) {
-  vi_df <- test_results %>%
-    extract_fit_parsnip() %>%
-    vip::vi()
+  vi_rank_agg <- test_results %>%
+    map(~ {
+      .x %>%
+        extract_fit_parsnip() %>%
+        vip::vi(rank = TRUE)
+    }) %>%
+    list_rbind(names_to = "fold_id") %>%
+    summarize(Mean_Importance = mean(Importance), .by = "Variable") %>%
+    mutate(Mean_Importance = dense_rank(Mean_Importance)) %>%
+    arrange(Mean_Importance)
 } else if (grepl("_svm", best_wflow)) {
   svm_pfun <- function(object, newdata) {
     kernlab::predict(object, new_data = newdata)[[".pred_class"]]
   }
   set.seed(2024)
-  vi_df <- test_results %>%
-    extract_fit_parsnip() %>%
-    vip::vi(
-      train = analysis(outer_fold),
-      method = "permute",
-      target = "class",
-      metric = "accuracy",
-      nsim = 5,
-      pred_wrapper = svm_pfun
-    )
+  vi_rank_agg <-
+    map2(test_results, outer_folds, ~ {
+      .x %>%
+        extract_fit_parsnip() %>%
+        vip::vi(
+          train = analysis(.y),
+          method = "permute",
+          target = "class",
+          metric = "accuracy",
+          nsim = 5,
+          pred_wrapper = svm_pfun,
+          rank = TRUE
+        )
+    }) %>%
+    list_rbind(names_to = "fold_id") %>%
+    summarize(Mean_Importance = mean(Importance), .by = "Variable") %>%
+    mutate(Mean_Importance = dense_rank(Mean_Importance)) %>%
+    arrange(Mean_Importance)
 }
 
 # Write all metrics to file
@@ -142,4 +164,4 @@ vi_file <- file.path(
   dataset,
   paste0("wflow", best_wflow, "_vi_", fold_id, "_", dataset, ".rds")
 )
-saveRDS(vi_df, vi_file)
+saveRDS(vi_rank_agg, vi_file)
