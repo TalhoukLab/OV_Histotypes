50d8bbce3b9aaeedd0cf1a894e1aae2594164b89
Derek Chiu <dchiu@bccrc.ca>
2024-03-08 17:22:06 -0800

    update retrain/2-merge_results.sh

--- pipeline/retrain/2-merge_results.sh
+++ pipeline/retrain/2-merge_results.sh
@@ -15,28 +15,33 @@ for dataset in "${dataSets[@]}"; do
     mkdir -p $shSubDir/$dataset
     mkdir -p $outputDir/$subDir
 
-    # Content of R file
-    R_file=$RSubDir/$dataset/retrain_merge.R
-    echo 'outputDir <- "'$outputDir'"' > $R_file
-    echo 'dataset <- "'$dataset'"' >> $R_file
-    echo 'inputDir <- "'$inputDir'"' >> $R_file
-    echo 'algs <- strsplit("'${algs[@]}'", " ")[[1]]' >> $R_file
-    echo 'source("pipeline/retrain/2-merge_results.R")' >> $R_file
+    for alg in "${algs[@]}"; do
+        # Content of R file
+        R_file=$RSubDir/$dataset/$alg.R
+        echo 'dataset <- "'$dataset'"' > $R_file
+        echo "n_folds <- $n_folds" >> $R_file
+        echo 'alg <- "'$alg'"' >> $R_file
+        echo 'rank_metric <- "'$rank_metric'"' >> $R_file
+        echo 'inputDir <- "'$inputDir'"' >> $R_file
+        echo 'outputDir <- "'$outputDir'"' > $R_file
+        echo 'inputDir <- "'$inputDir'"' >> $R_file
+        echo 'source("pipeline/retrain/2-merge_results.R")' >> $R_file
 
-    # Content of sh file
-    job_file=$shSubDir/$dataset/retrain_merge.sh
-    cat ./assets/sbatch_params.sh > $job_file
-    echo "cd $projDir" >> $job_file
-    echo "Rscript $R_file" >> $job_file
-    chmod +x $job_file
+        # Content of sh file
+        job_file=$shSubDir/$dataset/$alg.sh
+        cat ./assets/sbatch_params.sh > $job_file
+        echo "cd $projDir" >> $job_file
+        echo "Rscript $R_file" >> $job_file
+        chmod +x $job_file
 
-    # Add to queue if sbatch exists
-    if command -v sbatch &>/dev/null; then
-        file_to_submit+=($job_file)
-        echo -e "$GREEN_TICK Added to queue: $job_file"
-    else
-        bash $shell_file
-    fi
+        # Add to queue if sbatch exists
+        if command -v sbatch &>/dev/null; then
+            file_to_submit+=($job_file)
+            echo -e "$GREEN_TICK Added to queue: $job_file"
+        else
+            bash $shell_file
+        fi
+    done
 done
 
 # Submit to queue if sbatch exists
