f55fd9e82688ceb73824f0e52c759123048db20b
Derek Chiu <dchiu@bccrc.ca>
2024-03-09 10:23:02 -0800

    update two-step objects

--- pipeline/sequential/0-2S_setup.R
+++ pipeline/sequential/0-2S_setup.R
@@ -1,23 +1,27 @@
-# sequence
-step2_seq <- iv_summary_retrain_4 %>%
-  filter(measure == "macro_f1") %>%
-  transmute(
-    algorithm,
-    sampling,
-    f1_median = percentile_50
-  ) %>%
-  slice_max(order_by = f1_median) %>%
-  pull(algorithm)
+# 2-step algorithm setup
+library(dplyr)
+library(purrr)
 
-two_step_algs <- c(seq_top_c5[["algorithm"]], step2_seq)
-saveRDS(two_step_algs, here("data/two_step_algs.rds"))
+# Combine workflows
+seq_top_c5 <- readRDS(here("data/seq_top_c5.rds"))
+all_metrics_retrain_4 <- readRDS(here("data/all_metrics_retrain_4.rds"))
+rank_metric <- "f_meas"
+step2_wflow <-  all_metrics_retrain_4 %>%
+  filter(.metric == rank_metric, class_group == "Overall") %>%
+  slice_max(order_by = .estimate) %>%
+  pull(wflow)
 
-# data
-two_step_data <- list(train_step1_data,
-                      train_step2_data)
+two_step_wflows <- c(seq_top_c5[["wflow"]], step2_wflow)
+saveRDS(two_step_wflows, here("data/two_step_wflows.rds"))
+
+# Combine training data
+two_step_data <- map(c("train_step1_data", "train_step2_data"), ~ {
+  readRDS(here("data", paste0(.x, ".rds")))
+})
 saveRDS(two_step_data, here("data/two_step_data.rds"))
 
-# class
-two_step_class <- list(train_step1_class,
-                       train_step2_class)
+# Combine training classes
+two_step_class <- map(c("train_step1_class", "train_step2_class"), ~ {
+  readRDS(here("data", paste0(.x, ".rds")))
+})
 saveRDS(two_step_class, here("data/two_step_class.rds"))
