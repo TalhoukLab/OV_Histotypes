f31ee9d5266e863a8a4aec82b190fec759458814
Derek Chiu <dchiu@bccrc.ca>
2024-03-09 10:09:42 -0800

    update seq_data and seq_class

--- pipeline/sequential/0-seq_setup.R
+++ pipeline/sequential/0-seq_setup.R
@@ -1,23 +1,37 @@
-# workflows
-seq_top <- bind_rows(seq_top_c5,
-                     seq_top_c4,
-                     seq_top_c3,
-                     seq_top_c2)
+# Sequential algorithm setup
+library(dplyr)
+library(purrr)
+
+# Combine workflows
+seq_top <- map(5:2, ~ {
+  readRDS(here("data", paste0("seq_top_c", .x, ".rds")))
+}) %>%
+  list_rbind()
 seq_wflows <- seq_top[["wflow"]]
 saveRDS(seq_wflows, here("data/seq_wflows.rds"))
 
-# data
-seq_data <- list(train_data,
-                 retrain_4_data,
-                 retrain_3_data,
-                 retrain_2_data)
+# Combine training data
+seq_data <- map(
+  c(
+    "train_data",
+    "retrain_4_data",
+    "retrain_3_data",
+    "retrain_2_data"
+  ),
+  ~ readRDS(here("data", paste0(.x, ".rds")))
+)
 saveRDS(seq_data, here("data/seq_data.rds"))
 
-# class
-seq_class <- list(train_class,
-                  retrain_4_class,
-                  retrain_3_class,
-                  retrain_2_class) %>%
+# Combine training classes
+seq_class <- map(
+  c(
+    "train_class",
+    "retrain_4_class",
+    "retrain_3_class",
+    "retrain_2_class"
+  ),
+  ~ readRDS(here("data", paste0(.x, ".rds")))
+) %>%
   map2(seq_top[["class_group"]], ~ {
     if (n_distinct(.x) > 2) {
       ifelse(.x == .y, .x, "class_0")
