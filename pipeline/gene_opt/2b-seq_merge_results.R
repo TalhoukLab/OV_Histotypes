7b95794d3b811903cb8373ca5c84f2c3985994cd
Derek Chiu <dchiu@bccrc.ca>
2024-03-12 13:28:16 -0700

    refactor gene_opt seq_merge_results

---
+++ pipeline/gene_opt/2b-seq_merge_results.R
@@ -0,0 +1,95 @@
+# Load packages and data
+suppressPackageStartupMessages({
+  library(rlang)
+  library(parallel)
+  library(doParallel)
+  library(future)
+  library(themis)
+  library(tidymodels)
+  library(vip)
+  library(here)
+})
+source(here("src/funs.R"))
+source(here("pipeline/gene_opt/0b-setup_seq_data.R"))
+
+# Combined tuned sequential workflows across folds
+seq_wflows <- readRDS(file.path(inputDir, paste0(dataset, "_wflows.rds")))
+seq_wflow <- paste0(seq_wflows[nseq], "_s", nseq)
+
+tune_wflows_files <- list.files(
+  path = file.path(outputDir, "gene_opt", "sequential", "tune_wflows", dataset),
+  pattern = paste0(seq_wflow, "_add", ngene),
+  full.names = TRUE
+)
+
+tuned_set <- tune_wflows_files %>%
+  map(readRDS) %>%
+  do.call(rbind, .)
+tuned_set$wflow_id <- paste0(tuned_set$wflow_id, seq_len(n_folds))
+
+ranking <- tuned_set %>%
+  rank_results(rank_metric = rank_metric, select_best = TRUE) %>%
+  filter(.metric == rank_metric)
+
+# Best workflow
+best_wflow <- ranking[["wflow_id"]][1]
+
+# Best tuning parameters of workflow with highest metric
+best_params <- tuned_set %>%
+  extract_workflow_set_result(best_wflow) %>%
+  select_best(metric = rank_metric)
+
+# Finalize model with best set of tuning parameters
+best_model <- tuned_set %>%
+  extract_workflow(best_wflow) %>%
+  finalize_workflow(best_params)
+
+# Train best model on outer training sets, evaluate on outer test sets
+outer_folds <- folds$splits
+test_results <- map(outer_folds, ~ {
+  suppressMessages(last_fit(best_model, split = .x, metrics = mset))
+})
+
+# Test set predictions
+test_preds <- test_results %>%
+  map(collect_predictions) %>%
+  list_rbind(names_to = "fold_id")
+
+# Select predicted probability columns for multiclass/binary AUC
+prob_cols <- test_preds %>%
+  select(matches(".pred(?!_class)", perl = TRUE)) %>%
+  names()
+if (n_distinct(class) == 2) {
+  prob_cols <- prob_cols[1]
+}
+
+# Calculate overall metrics
+overall_metrics <- test_preds %>%
+  mset(truth = class, prob_cols, estimate = .pred_class) %>%
+  add_column(class_group = "Overall") %>%
+  suppressWarnings()
+
+# Don't interpret F-measure if some levels had no predicted events
+overall_metrics <- tryCatch({
+  test_preds %>%
+    mset(truth = class, prob_cols, estimate = .pred_class) %>%
+    add_column(class_group = "Overall")
+}, warning = function(w) {
+  overall_metrics %>%
+    mutate(.estimate = ifelse(.metric == "f_meas", NA_real_, .estimate))
+})
+
+# Combine all metrics
+all_metrics <- overall_metrics %>%
+  arrange(.metric)
+
+# Write all metrics to file
+metrics_file <- file.path(
+  outputDir,
+  "gene_opt",
+  "sequential",
+  "merge_results",
+  dataset,
+  paste0(seq_wflow,  "_add", ngene, "_metrics_", dataset, ".rds")
+)
+saveRDS(all_metrics, metrics_file)
