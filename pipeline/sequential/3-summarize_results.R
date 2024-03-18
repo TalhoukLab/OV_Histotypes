8c1655a010b6a3a7747fc5a26e572a458a6d0588
Derek Chiu <dchiu@bccrc.ca>
2024-03-17 22:45:16 -0700

    combine per-class metrics for sequential classifier

--- pipeline/sequential/3-summarize_results.R
+++ pipeline/sequential/3-summarize_results.R
@@ -27,25 +27,25 @@ all_models_file <- file.path(
 )
 saveRDS(all_models, all_models_file)
 
-# Summarize all workflow metrics and write to file
+# Combine per-class metrics and write to file
 metrics_files <- list.files(
   path = file.path(outputDir, "sequential", "merge_results", dataset),
-  pattern = "metrics",
+  pattern = "per_class_metrics",
   full.names = TRUE
 )
-all_wflow_metrics <- metrics_files %>%
+per_class_metrics <- metrics_files %>%
   set_names(gsub("_metrics.*", "", basename(.))) %>%
   map(readRDS) %>%
-  list_rbind(names_to = "wflow")
+  list_rbind()
 
-all_metrics_file <- file.path(
+per_class_metrics_file <- file.path(
   outputDir,
   "sequential",
   "summarize_results",
   dataset,
-  paste0("all_metrics_", dataset, ".rds")
+  paste0("per_class_metrics_", dataset, ".rds")
 )
-saveRDS(all_wflow_metrics, all_metrics_file)
+saveRDS(per_class_metrics, per_class_metrics_file)
 
 # Summarize all workflow variable importance ranks and write to file
 vi_files <- list.files(
