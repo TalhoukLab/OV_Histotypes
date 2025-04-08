66fe42ec6c5537bc9bbb01348fd796bd582a9efc
Derek Chiu <dchiu@bccrc.ca>
2025-04-08 16:28:51 -0700

    only use ranked_vi from training set

--- pipeline/gene_opt/0a-setup_data.R
+++ pipeline/gene_opt/0a-setup_data.R
@@ -9,12 +9,7 @@ base_genes <- c("COL11A1", "CD74", "CD2", "TIMP3", "LUM", "CYTIP", "COL3A1",
                 "CDKN3", "BRCA2", "CEACAM5", "ANXA4", "SERPINE1", "CRABP2", "DNAJC9")
 
 # Import ranked variable importance for trained workflows
-ranked_vi <- readRDS(file.path(
-  outputDir,
-  "summarize_results",
-  dataset,
-  paste0("all_vi_", dataset, ".rds")
-))
+ranked_vi <- readRDS(file.path(outputDir, "summarize_results", "train", "all_vi_train.rds"))
 
 # Order of candidate genes for top workflow
 top_wflow <- readRDS(here("data/seq_top_c5.rds"))[["wflow"]]
