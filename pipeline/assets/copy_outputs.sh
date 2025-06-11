fd5da2e0f90effb9d48abaf0ceaae2638fda258a
Derek Chiu <dchiu@bccrc.ca>
2025-06-11 16:58:56 -0700

    helper script to copy outputs from server to repo

---
+++ pipeline/assets/copy_outputs.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Directory paths
+user="$(whoami)"
+projDir="/home"/$user"/Projects/OV_Histotypes"
+inputDir=$projDir/"data"
+resultsDir="/projects/"$user"_prj/results/OV_Histotypes_v3"
+outputDir=$resultsDir"/outputs"
+
+# Training pipeline outputs
+cp $outputDir/summarize_results/train/* $inputDir/
+
+# Retraining pipeline outputs
+cp $outputDir/retrain/summarize_results/retrain_4/* $inputDir/
+cp $outputDir/retrain/summarize_results/retrain_3/* $inputDir/
+cp $outputDir/retrain/summarize_results/retrain_2/* $inputDir/
+
+# Sequential pipeline outputs
+cp $outputDir/sequential/summarize_results/seq/* $inputDir/
+cp $outputDir/sequential/summarize_results/two_step/* $inputDir/
+
+# Gene optimization pipeline outputs
+cp $outputDir/gene_opt/summarize_results/conf/* $inputDir/
+
+# Sequential gene optimization pipeline outputs
+cp $outputDir/gene_opt/sequential/summarize_results/conf_seq/* $inputDir/
+cp $outputDir/gene_opt/sequential/summarize_results/conf_two_step/* $inputDir/
